4bd6aaaa3a32b56a9c5637194684c489ea22f277
kamalbakhouche <kamalbakhouche@gmail.com>
2018-12-04 18:52:13 -0500

    Add files via upload

---
+++ DataSet1.cs
@@ -0,0 +1,12 @@
+﻿namespace Etat_civil {
+    
+    
+    public partial class DataSet1 {
+    }
+}
+namespace Etat_civil {
+    
+    
+    public partial class DataSet1 {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Etat_civil.Layouts;
+using Etat_civil.Layouts.Admin;
+using Etat_civil.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Etat_civil
+{
+    static class Program
+    {
+        public static Authentification auth;
+        public static Main main;
+        public static User user;
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            auth = new Authentification();
+            main = new Main();
+            Application.Run(auth);
+        }
+    }
+}
